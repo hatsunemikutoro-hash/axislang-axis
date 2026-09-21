909a4374d0647373b69ea8dff711edea55041c03
nightzin <hatsunemikutoro@gmail.com>
2026-09-21 16:21:45 -0300

    Bug hunt commit

--- src/interpreter.c
+++ src/interpreter.c
@@ -9,25 +9,78 @@ Interpreter *interpreter_create(const char *filename)
 {
     Interpreter *interpret = malloc(sizeof(Interpreter));
 
+    if (interpret == NULL)
+    {
+        fprintf(stderr, "Internal Error: Failed to allocate interpreter\n");
+        free(interpret);
+        return NULL;
+    }
+
     Lexer *lexer = malloc(sizeof(Lexer));
+
+    if (lexer == NULL)
+    {
+        fprintf(stderr, "Internal Error: Failed to allocate lexer\n");
+        free(interpret);
+        return NULL;
+    }
     lexer->size = 0;
     lexer->line = 1;
     lexer->c = read_file(filename);
 
+    if (lexer->c == NULL)
+    {
+        free(lexer);
+        free(interpret);
+        return NULL;
+    }
+
     Parser *parser = malloc(sizeof(Parser));
+
+    if (parser == NULL)
+    {
+        fprintf(stderr, "Internal Error: Failed to allocate parser\n");
+        free(lexer->c);
+        free(lexer);
+        free(interpret);
+        return NULL;
+    }
     parser->lexer = lexer;
     advance(parser);
 
     interpret->parser = parser;
     interpret->vm = vm_create();
 
+    if (interpret->vm == NULL)
+    {
+        fprintf(stderr, "Axis error: failed to allocate virtual machine\n");
+        free(parser);
+        free(lexer->c);
+        free(lexer);
+        free(interpret);
+        return NULL;
+    }
+
     interpret->program = parse_program(parser);
+
+    if (interpret->program == NULL)
+    {
+        fprintf(stderr, "Axis error: failed to parse program\n");
+        vm_destroy(interpret->vm);
+        free(parser);
+        free(lexer->c);
+        free(lexer);
+        free(interpret);
+        return NULL;
+    }
 
     return interpret;
 }
 
-void interpreter_destroy(Interpreter* interp) {
-    if  (interp == NULL) {
+void interpreter_destroy(Interpreter *interp)
+{
+    if (interp == NULL)
+    {
         return;
     }
     program_destroy(interp->program);
@@ -40,7 +93,7 @@ void interpreter_destroy(Interpreter* interp) {
     free(interp);
 }
 
-void interpreter_run(Interpreter* interp) {
+void interpreter_run(Interpreter *interp)
+{
     vm_run(interp->vm, interp->program);
-    
 }
--- src/openfile.c
+++ src/openfile.c
@@ -1,27 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *read_file(const char *filename) {
+char *read_file(const char *filename)
+{
     FILE *fp = fopen(filename, "r");
 
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Axis error: cannot open  file %s\n", filename);
         return NULL;
     }
 
-    fseek(fp, 0, SEEK_END);
+    if (fseek(fp, 0, SEEK_END) != 0)
+    {
+        fprintf(stderr, "Axis error: cannot seek file '%s'\n", filename);
+        fclose(fp);
+        return NULL;
+    };
+
     long size = ftell(fp);
+
+    if (size < 0)
+    {
+        fprintf(stderr, "Axis error: cannot determine file size \n");
+        fclose(fp);
+        return NULL;
+    }
+
     rewind(fp);
 
     char *content = malloc(size + 1);
 
-    if (content == NULL) {
+    if (content == NULL)
+    {
+        fprintf(stderr, "Axis error: out of memory\n");
         fclose(fp);
         return NULL;
     }
-    
-    fread(content, 1, size, fp);
-    content[size] = '\0';
 
+    size_t bytes_read = fread(content, 1, (size_t)size, fp);
+
+    if (bytes_read != (size_t)size)
+    {
+        fprintf(stderr, "Axis error: failed to read file '%s'\n", filename);
+        free(content);
+        fclose(fp);
+        return NULL;
+    }
+
+    content[bytes_read] = '\0';
     fclose(fp);
     return content;
 }
